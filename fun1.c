4ea0bb49d24b274bd28a0ff1bdbe2c33d79bde24
mohamed adel <adel762dp47@gmail.com>
2023-10-16 12:32:16 -0700

    message

--- fun1.c
+++ fun1.c
@@ -44,3 +44,35 @@ int MO_str(char *s)
 	}
 	return (i);
 }
+
+/**
+ * MO_help - the function
+ * @d: int1
+ * @a: int2
+ * @x: int2
+ *
+ * Return: inteeger
+ */
+
+int MO_help(int d, int a, int x)
+{
+	
+}
+
+/**
+ * MO_int - print inteeger
+ * @list: input
+ *
+ * Return:inteeger
+ */
+int MO_int(va_list **list)
+{
+	int a = va_arg(**list, int);
+	char c = 48;
+	int x = 0, i = 0, d = 1e9;
+
+	if (a >= INT_MIN && a <= INT_MAX)
+	{
+		if (
+	}
+}
